fe4b0df023193a068f40e5475792c5a891e2705b
Dhar <dhar@fyles-MacBook-Air.local>
2021-04-15 15:36:07 +0530

    Updated changes

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/<path:path>')
 def home():
-    return send_from_directory('js', path)
+    return send_from_directory('static', path)
 
 @app.route('/sign_up', methods=['POST'])
 def signUpUserForNewsletter():
-    body = request.get_json()
+    body = request.form
     print(body)
     return "<h1>Success!</h1>"
